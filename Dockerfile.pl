fa298047f43dfb24c625bbc54bff1c8f074756f5
przemeksokoleoko <59530781+przemeksokoleoko@users.noreply.github.com>
2026-02-08 14:18:16 +0100

    Upgrade Dockerfile to Python 3.12 and enhance setup
    Updated Dockerfile to use Python 3.12 and added necessary packages for the application. Configured environment variables and entry point for the matter server.

--- Dockerfile.pl
+++ Dockerfile.pl
@@ -1,12 +1,50 @@
-# This is a sample Dockerfile
+FROM python:3.12-slim-bookworm
 
-FROM python:3.8-slim
+# Set shell
+SHELL ["/bin/bash", "-o", "pipefail", "-c"]
 
 WORKDIR /app
 
-COPY requirements.txt .
-RUN pip install --no-cache-dir -r requirements.txt
+RUN \
+    set -x \
+    && apt-get update \
+    && apt-get install -y --no-install-recommends \
+        curl \
+        libuv1 \
+        zlib1g \
+        libjson-c5 \
+        libnl-3-200 \
+        libnl-route-3-200 \
+        unzip \
+        gdb \
+        iputils-ping \
+        iproute2 \
+    && apt-get purge -y --auto-remove \
+    && rm -rf \
+        /var/lib/apt/lists/* \
+        /usr/src/*
 
-COPY . .
+ARG PYTHON_MATTER_SERVER
 
-CMD ["python", "app.py"]
+ENV chip_example_url "https://github.com/przemeksokoleoko/matter-linux-ota-provider/releases/download/v2026.1.0"
+ARG TARGETPLATFORM
+
+RUN \
+    set -x \
+    && echo "${TARGETPLATFORM}" \
+    && if [ "${TARGETPLATFORM}" = "linux/armv7" ]; then \
+        curl -Lo /usr/local/bin/chip-ota-provider-app "${chip_example_url}/chip-ota-provider-app-aarch64"; \
+    else \
+        exit 1; \
+    fi \
+    && chmod +x /usr/local/bin/chip-ota-provider-app
+
+# hadolint ignore=DL3013
+RUN \
+    pip3 install --no-cache-dir "python-matter-server[server]==${PYTHON_MATTER_SERVER}"
+
+VOLUME ["/data"]
+EXPOSE 5580
+
+ENTRYPOINT [ "matter-server" ]
+CMD [ "--storage-path", "/data", "--paa-root-cert-dir", "/data/credentials" ]
